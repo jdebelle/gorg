bf7f51ea2b5c72ef3b9acd5aed81b4b5cc7f51a3
Jonathan de Belle <jonathan.debelle@gmail.com>
2021-01-31 14:34:57 -0500

    Remove reference to tutorial in help text
     - Add TODO note to add it back later

--- gorg-cli/inc/help.h
+++ gorg-cli/inc/help.h
@@ -3,12 +3,17 @@
 #include <string>
 #include <filesystem>
 
+
+//
+//	TODO - Add tutorial and reference to tutorial:  
+//		aka: primary commands (see also: gorg help tutorial)
+//
 const std::string help_main =
 	"usage gorg: [--version|-v] [--help|-h] <command> [<args>]\n"
 	"\n"
 	"These are common Gorg commands in various situations:\n"
 	"\n"
-	"primary commands (see also: gorg help tutorial)\n"
+	"primary commands\n"
 	"   asset       Create, edit or validate a gorg asset\n"
 	"   generate    Generate searchable html index\n"
 	"\n"
